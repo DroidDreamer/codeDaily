c019f6d2a678eae75971fe29c30fe2c3947ca091
eric <showme11211@63.com>
2018-07-26 00:06:25 +0800

    add Fastfile

--- Generic type inference.playground/Contents.swift
+++ Generic type inference.playground/Contents.swift
@@ -4,9 +4,14 @@ import UIKit
     print(11111)
 }
 
+//age <- dict["age"]
+//let num = dict["age"]
+//guard let numm = num as? type(of: age){
+//
+//}
 public class Person {
     var age: Int = 2
-    
+
      func num<T>()-> T? {
         if let num = age as? T {
             print(2222)
@@ -18,11 +23,19 @@ public class Person {
     }
 }
 
+func count1<Num>(left: Num, right: Num){
+}
 
-
-func count<T>(left: T, right: Person){
+func count<T: Numeric>(left: T, right: Person){
     basicType(left, object: right.num())
 }
 
 let boy = Person()
 count(left: 3, right: boy)
+
+
+
+
+
+
+
